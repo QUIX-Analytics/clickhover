f14387d25c0b85a38f786b0ecbca35095dff4163
Taylor Munnerlyn <taylormunnerlyn@gmail.com>
2016-07-20 12:15:45 -0600

    changed auth so that everything that is created or submitted from the auth page is converted toLowerCase

--- src/client/app/auth/auth.controller.js
+++ src/client/app/auth/auth.controller.js
@@ -9,15 +9,19 @@
   function AuthController($scope, authService, $state) {
     var vm = this;
     vm.login = function(user) {
+      user.email = user.email.toLowerCase();
       authService.login(user).then(function(response) {
         console.log(response);
+        user = {};
         $state.go('dashboard');
       })
     }
 
     vm.register = function(newUser) {
+      newUser.email = newUser.email.toLowerCase()
       authService.register(newUser).then(function(response) {
         console.log(response);
+        newUser = {};
         alert('Account Successfully Created')
       })
     }
