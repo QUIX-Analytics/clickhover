441fe78b60b68cdf416af2482b20d64fb11ec6dc
Taylor Munnerlyn <taylormunnerlyn@gmail.com>
2016-07-20 11:27:41 -0600

    created register success alert and $state.go on login

--- src/client/app/auth/auth.controller.js
+++ src/client/app/auth/auth.controller.js
@@ -6,7 +6,7 @@
     .module('quix.auth')
     .controller('AuthController', AuthController);
 
-  function AuthController($scope, authService) {
+  function AuthController($scope, authService, $state) {
     var vm = this;
     vm.login = function(user) {
       authService.login(user).then(function(response) {
@@ -17,6 +17,8 @@
     vm.register = function(newUser) {
       authService.register(newUser).then(function(response) {
         console.log(response);
+        alert('Account Successfully Created')
+        $state.go('dashboard');
       })
     }
 
