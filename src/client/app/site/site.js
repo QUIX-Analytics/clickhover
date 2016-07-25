940a7312231dec3ffa2d4b700c775079032693c1
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-07-25 11:52:53 -0600

    testing site module

--- src/client/app/site/site.js
+++ src/client/app/site/site.js
@@ -3,18 +3,19 @@
   'use strict';
   angular
     .module('quix.site')
-    .controller('Site', Site)
+    .controller('Site', Site);
 
   function Site($scope, siteService) {
 
     var vm = this;
+
     vm.getData = function() {
       siteService.getData().then(function(response) {
         console.log(response);
         vm.data = response.data;
       })
-    }
-    vm.getData();
+    };
+    // vm.getData();
 
 
   }
