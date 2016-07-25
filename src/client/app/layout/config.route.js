fcb1e6688a4cff94b0ca51e9b51416c462ba3c95
James Putnam <jonathan.putnam86@gmail.com>
2016-07-25 16:30:56 -0600

    login ready to be pushed

--- src/client/app/layout/config.route.js
+++ src/client/app/layout/config.route.js
@@ -1,29 +1,29 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('quix.layout')
-        .run(appRun);
-
-    // appRun.$inject = ['routerHelper']
-
-    /* @ngInject */
-    function appRun(routerHelper) {
-        routerHelper.configureStates(getStates());
-    }
-
-    function getStates() {
-        return [
-            {
-
-                state: 'dashboard',
-                config: {
-                    url: '/dashboard',
-                    templateUrl: '/app/layout/dashboard.html',
-                    controller: 'Dashboard',
-                    controllerAs: 'Dashboard'
-                }
-            }
-        ];
-    }
-})();
+// (function() {
+//     'use strict';
+//
+//     angular
+//         .module('quix.layout')
+//         .run(appRun);
+//
+//     // appRun.$inject = ['routerHelper']
+//
+//     /* @ngInject */
+//     function appRun(routerHelper) {
+//         routerHelper.configureStates(getStates());
+//     }
+//
+//     function getStates() {
+//         return [
+//             {
+//
+//                 state: 'dashboard',
+//                 config: {
+//                     url: '/dashboard',
+//                     templateUrl: '/app/layout/dashboard.html',
+//                     controller: 'Dashboard',
+//                     controllerAs: 'Dashboard'
+//                 }
+//             }
+//         ];
+//     }
+// })();
