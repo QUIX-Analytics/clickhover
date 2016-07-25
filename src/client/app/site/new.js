ce29b0d6496c267df60a1162caf71f8950644705
Jared M. Peterson <jared@peteyinc.com>
2016-07-25 13:47:17 -0600

    add site function

--- src/client/app/site/new.js
+++ src/client/app/site/new.js
@@ -8,13 +8,18 @@
   function New($scope, siteService) {
 
     var vm = this;
-    vm.getData = function() {
-      siteService.getData().then(function(response) {
-        console.log(response);
-        vm.data = response.data;
+
+    vm.newSite = function(site) {
+      console.log(site.URL);
+      // site.URL = site.URL.toLowerCase()
+
+      siteService.addSite(site).then(function(response) {
+        site = {};
+        console.log(site);
+        alert('Success!');
+        $state.go('dashboard');
       })
     }
-    vm.getData();
 
 
   }
