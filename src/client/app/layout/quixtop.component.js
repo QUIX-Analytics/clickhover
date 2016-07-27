61ad86f922d0f9de18ab7783dbfe6efc255db539
Brett Caudill <brett.caudillo@gmail.com>
2016-07-27 14:37:37 -0600

    Revert "Navcontrollers"

--- src/client/app/layout/quixtop.component.js
+++ src/client/app/layout/quixtop.component.js
@@ -5,8 +5,6 @@
   angular
     .module('quix.layout')
     .component('quixtop', {
-      templateUrl: '/app/layout/quixtop.html',
-			controller: 'Quixtop',
-			controllerAs: 'Quixtop'
+      templateUrl: '/app/layout/quixtop.html'
     })
 })()
