b1af4c5af6042111fd9f1dfe96866aa41f849595
asBrettisay <brett.caudill.is@gmail.com>
2016-07-28 12:39:15 -0600

    dataService will use sessionStorage
    This will store user information and prevent unnecessary queries to the
    server.
    
    This will also persist sessions in between node refreshes.

--- src/client/app/core/dataservice.js
+++ src/client/app/core/dataservice.js
@@ -1,27 +1,29 @@
 (function () {
 
+
 	angular.module('quix.core')
 		.factory('dataService', dataService);
 
 	function dataService($http, $q) {
-		var currentUser;
 
 		var service = {
-			getUser: getUser
+			getUser: getUser,
 		}
 
 		return service;
 
 		////////////////////////////////////////////////
 
+
 		function getUser() {
+      var currentUser = JSON.parse(sessionStorage.getItem('quixUser'));
 			if(!currentUser) {
 	      return $http({
 	        method: 'GET',
 	        url: '/auth/me'
-	      }).then(function(response) {
-					currentUser = response.data;
-					return currentUser;
+	      }).then(function success(response) {
+					sessionStorage.setItem('quixUser', JSON.stringify(response.data));
+					return response.data;
 	    	});
 			}
 			else {
