0caa98edabf0262482ab2d3374a57b274b8d9a00
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-08-02 02:07:52 -0600

    double quotes are for html. single quotes for javascript

--- src/client/app/core/dataservice.js
+++ src/client/app/core/dataservice.js
@@ -66,37 +66,37 @@
 		}
 
 		function sideNav(){
-			var subNav = document.getElementsByTagName("quixsub")[0];
-			var uiView = document.getElementsByTagName("ui-view")[0];
-			var toggleBtn = document.getElementById("subnav-toggle");
-			var arrowBtn = document.getElementById("subnav-arrow");
-			var maxW = "15rem";
-			var maxT = "15rem";
+			var subNav = document.getElementsByTagName('quixsub')[0];
+			var uiView = document.getElementsByTagName('ui-view')[0];
+			var toggleBtn = document.getElementById('subnav-toggle');
+			var arrowBtn = document.getElementById('subnav-arrow');
+			var maxW = '15rem';
+			var maxT = '15rem';
 
 			if (subNav.style.width === maxW){
-				subNav.style.width = "0px";
-				uiView.style.marginLeft = "5.5rem";
-				toggleBtn.style.left = "0rem";
-        arrowBtn.style.transform = "rotate(180deg)";
+				subNav.style.width = '0px';
+				uiView.style.marginLeft = '5.5rem';
+				toggleBtn.style.left = '0rem';
+        arrowBtn.style.transform = 'rotate(180deg)';
 
 			} else {
 				subNav.style.width = maxW;
-				uiView.style.marginLeft = "20.5rem";
+				uiView.style.marginLeft = '20.5rem';
 				toggleBtn.style.left = maxT;
-        arrowBtn.style.transform = "rotate(0deg)";
+        arrowBtn.style.transform = 'rotate(0deg)';
 			}
 		}
 
 		function addSiteNav(){
-			var uiView = document.getElementsByTagName("ui-view")[0];
-			var subNav = document.getElementsByTagName("quixsub")[0];
-			var toggleBtn = document.getElementById("subnav-toggle");
-			var arrowBtn = document.getElementById("subnav-arrow");
-			if (uiView.style.marginLeft === "5.5rem"){
-				uiView.style.marginLeft = "20.5rem";
-				toggleBtn.style.left = "15rem";
-				arrowBtn.style.transform = "rotate(0deg)";
-				subNav.style.width = "15rem";
+			var uiView = document.getElementsByTagName('ui-view')[0];
+			var subNav = document.getElementsByTagName('quixsub')[0];
+			var toggleBtn = document.getElementById('subnav-toggle');
+			var arrowBtn = document.getElementById('subnav-arrow');
+			if (uiView.style.marginLeft === '5.5rem'){
+				uiView.style.marginLeft = '20.5rem';
+				toggleBtn.style.left = '15rem';
+				arrowBtn.style.transform = 'rotate(0deg)';
+				subNav.style.width = '15rem';
 
 			}
 		}
