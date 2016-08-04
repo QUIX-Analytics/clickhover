60466f27e49e89c385cd49a73b073e8c384ab130
James Putnam <jonathan.putnam86@gmail.com>
2016-08-04 00:39:20 -0600

    piechart reacting with uiView not real data

--- src/client/app/site/pieChart.js
+++ src/client/app/site/pieChart.js
@@ -7,60 +7,65 @@
       function pieChart() {
         var directive = {
             link:link,
-            templateUrl: 'pieChart.html',
-            restrict: 'EA',
-            controller: Dashboard
+            // templateUrl: 'pieChart.html',
+            restrict: 'EA'
         };
         return directive;
 
         function link(scope, el, attrs){
-        d3.select(el[0]).append('svg')
-        var width = (475*1.5),
-        height = (250*1.5),
-        radius = Math.min(width, height) / 2;
+          d3.select(el[0]).append('svg')
+          var width = (475*1.5),
+          height = (250*1.5),
+          outerRadius = 100,
+          radius = Math.min(width, height) / 2;
 
-        var data = [
-            { age: '<5', population: 2704659 },
-            { age: '5-13', population: 4499890 },
-            { age: '14-17', population: 2159981 },
-            { age: '18-24', population: 3853788 },
-            { age: '25-44', population: 14106543 },
-            { age: '45-64', population: 8819342 },
-            { age: '>65', population: 2612463},
-            {age: '<100', population: 949840}
-        ];
-        var color = d3.scale.ordinal()
-            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#000"]);
-        var arc = d3.svg.arc()
-          .outerRadius(radius - 10)
-          .innerRadius(radius - 70);
-        var labelArc = d3.svg.arc()
-            .outerRadius(radius - 20)
-            .innerRadius(radius - 20)
-        var pie = d3.layout.pie()
-            .sort(null)
-            .value(function(d) { return d.population})
-        var svg = d3.select('body').append('svg')
-            .attr('height', height)
-            .attr('width', width)
-          .append('g')
-            .attr('transform', 'translate(' +width/2+ ',' + height/2+')');
+          var data = [
+              { browser: 'Chrome', population: 45 },
+              { browser: 'Safari', population: 20 },
+              { browser: 'FireFox', population: 15 },
+              { browser: 'IE', population: 9 },
+              { browser: 'Other', population: 13 }
+          ];
+          var color = d3.scale.ordinal()
+              .range(["#F0991F","#437DCC", "#E62117", "#6b486b", "#09AA64"]);
+          var arc = d3.svg.arc()
+            .outerRadius(radius - 10)
+            .innerRadius(radius - 100)
+          var labelArc = d3.svg.arc()
+              .outerRadius(radius - 60)
+              .innerRadius(radius - 60)
+          var pie = d3.layout.pie()
+              .sort(null)
+              .value(function(d) { return d.population})
+          var svg = d3.select(el[0]).append('svg')
+              .attr('height', height)
+              .attr('width', width)
+            .append('g')
+              .attr('transform', 'translate(' +width/2+ ',' + height/2+')');
 
-        var g = svg.selectAll('.arc')
-            .data(pie(data))
-          .enter().append('g')
-            .attr('class', 'arc');
+          var g = svg.selectAll('.arc')
+              .data(pie(data))
+            .enter().append('g')
+              .attr('class', 'arc');
 
-        g.append('path')
-            .attr('d', arc)
-            .attr('fill', function(d) {return color(d.data.age)});
+          g.append('path')
+              .attr('d', arc)
+              .attr('stroke', "#212326")
+              .attr('stroke-width', "4")
+              .attr('fill', function(d) {return color(d.data.browser)});
 
-        g.append('text')
-          .text(function(d){ return d.data.age})
-          .attr('transform', function(d){
-            return 'translate('+labelArc.centroid(d)+')'
-      });
-      }
+          g.append('text')
+            .text(function(d){
+              return d.data.browser})
+              .attr("text-anchor", "middle") //center the text on it's origin
+              .style("fill", "White")
+              .style("font", "14px")
+              .attr('transform', function(d){
+                d.outerRadius = outerRadius - 50;
+                d.innerRadius = outerRadius - 45;
+              return 'translate('+labelArc.centroid(d)+')'
+            });
+        }
 
     }
 
